cde0e5310f91f5053ba94e172ddb02c3a876d62b
yams <licface13@gmail.com>
2020-05-25 21:12:40 +0700

    version: 3.24 ~ 2020-05-25 21:12:36:955000

--- setup.py
+++ setup.py
@@ -10,8 +10,6 @@ except:
     pass
 shutil.copy2('__version__.py', 'make_colors')
 
-with io.open("README.rst", "rt", encoding="utf8") as f:
-    readme = f.read()
 
 # with io.open("__version__.py", "rt", encoding="utf8") as f:
     # version = re.search(r"version = \'(.*?)\'", f.read()).group(1)
@@ -33,8 +31,8 @@ setup(
     maintainer="cumulus13 Team",
     maintainer_email="cumulus13@gmail.com",
     description="Make command line text colored",
-    long_description=readme,
-    long_description_content_type = "text/markdown", 
+    #long_description_content_type="text/markdown", 
+    #long_description=readme,
     packages=["make_colors"],
     data_files=['__version__.py', 'README.rst', 'LICENSE.rst'],
     include_package_data=True,
