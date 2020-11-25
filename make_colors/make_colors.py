284c99630dd716ea7484297bcb8e48263fa6a94e
yams <licface13@gmail.com>
2020-11-25 17:29:11 +0700

    version: 3.29 ~ 2020-11-25 17:29:09:840860

--- make_colors/make_colors.py
+++ make_colors/make_colors.py
@@ -1,6 +1,15 @@
 import os
 import sys
 import re
+if sys.platform == 'win32':
+    import ctypes
+    kernel32 = ctypes.WinDLL('kernel32')
+    hStdOut = kernel32.GetStdHandle(-11)
+    mode = ctypes.c_ulong()
+    kernel32.GetConsoleMode(hStdOut, ctypes.byref(mode))
+    mode.value |= 4
+    kernel32.SetConsoleMode(hStdOut, mode)
+    
 class Win10Colors(object):
     """docstring for Win10Colors"""
     def __init__(self):
